7bb446f3b3bb4603711ce28d7083525d1aee5a25
pavan0209 <pavanasonawane@gmail.com>
2026-07-19 17:04:39 +0530

    added reusable comfirm alert dialog

---
+++ src/app/shared/components/app-confirm-dialog/app-confirm-dialog.ts
@@ -0,0 +1,34 @@
+import { Component, inject } from '@angular/core';
+
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+export interface ConfirmDialogData {
+  title: string;
+  message: string;
+  confirmButtonText: string;
+  cancelButtonText: string;
+}
+
+@Component({
+  selector: 'app-confirm-dialog',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule, MatIconModule],
+  templateUrl: './app-confirm-dialog.html',
+  styleUrl: './app-confirm-dialog.scss',
+})
+export class AppConfirmDialog {
+  readonly dialogRef = inject(MatDialogRef<AppConfirmDialog>);
+
+  readonly data = inject<ConfirmDialogData>(MAT_DIALOG_DATA);
+
+  confirm(): void {
+    this.dialogRef.close(true);
+  }
+
+  cancel(): void {
+    this.dialogRef.close(false);
+  }
+}
