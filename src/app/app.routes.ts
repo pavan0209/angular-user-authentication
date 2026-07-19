fb1a0534e983314e9f9cf31a5bd299e4353b94d1
pavan0209 <pavanasonawane@gmail.com>
2026-07-19 11:46:38 +0530

    added app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
+import { Dashboard } from './features/auth/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'register',
+    component: Register,
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
